4be3a789b166975b04a57b7922fdf5fc459da03b
Restuta <restuta8@gmail.com>
2016-12-27 14:31:16 -0800

    fixing perf issue with a hack for now (opened issue for original lib https://github.com/spiermar/d3-flame-graph/issues/65)

--- scripts/d3.flameGraph.js
+++ scripts/d3.flameGraph.js
@@ -73,8 +73,8 @@
       // and with a warm palette.
       var vector = 0;
       if (name) {
-        name = name.replace(/.*`/, "");		// drop module name if present
-        name = name.replace(/\(.*/, "");	// drop extra info
+        // name = name.replace(/.*`/, "");		// drop module name if present
+        // name = name.replace(/\(.*/, "");	// drop extra info
         vector = generateHash(name);
       }
       var r = 200 + Math.round(55 * vector);
